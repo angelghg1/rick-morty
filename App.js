c4062befdc65101d95d7331777ce8a05d4a9f0d0
angelghg1 <angelghg0211@gmail.com>
2024-05-22 12:17:49 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+//import logo from './logo.svg';
+import image1 from '../src/img/rick-morty.png';
+import { useState } from 'react';
+import Characters from './components/Characters';
+import './App.css';
+
+function App() {
+  const[ characters, setCharacter] = useState(null);  
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character")
+  console.log(api);
+  const characterApi = await api.json();
+  console.log( characterApi);
+  console.log(characters);
+  setCharacter(characterApi.results);
+  console.log(characters);
+}
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title"> Rick & Morty</h1>
+        {characters ? (
+          <Characters characters={characters}></Characters>
+      ):(
+        <>
+        <img src={image1} className="img-home" alt="rick and morty" />
+        <button onClick={reqApi} className="btn-search">
+          Buscar personaje
+        </button>
+        </>
+        )
+      }
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
